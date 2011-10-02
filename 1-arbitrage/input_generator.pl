3e867ddf48157b86a2da6c053a2ea6c51627f262
William Wolf <throughnothing@gmail.com>
2011-10-02 19:48:52 -0400

    perl input generator

---
+++ 1-arbitrage/input_generator.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/env perl
+use v5.14;
+
+my $n = $ARGV[0] or die "Must provide a dimension n";
+open my $out, '>generated_input.txt';
+
+sub disp{ 
+    print $out $_[0];
+    print $_[0];
+};
+
+disp("$n\n");
+for(1 .. $n-1){
+    for(1 .. $n-1){
+        disp(rand() + .03 . ' ');
+    }
+    disp("\n");
+}
